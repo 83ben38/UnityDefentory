11218d555c57878a58be2c499c11bdc42c93d8d0
Ben <bb14738@ms.wasatch.edu>
2025-06-04 13:34:01 -0600

    day 5

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -1,17 +1,37 @@
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceManager : MonoBehaviour
 {
     public Dictionary<Shape.Type,int> resources = new();
     public static ResourceManager instance;
+    public List<Sprite> shapes;
+    private Dictionary<Shape.Type,TextMeshProUGUI> texts = new();
+    public GameObject childPrefab;
     private void Awake()
     {
         instance = this;
     }
 
-    private void Start()
+    private void Update()
     {
-        
+        foreach (var type in resources.Keys)
+        {
+            if (!texts.ContainsKey(type))
+            {
+                GameObject go = Instantiate(childPrefab, transform);
+                texts[type] = go.GetComponentInChildren<TextMeshProUGUI>();
+                go.GetComponent<Image>().sprite = shapes[(int)type];
+            }
+            else
+            {
+                Debug.Log(texts[type]);
+                Debug.Log(resources[type]);
+                texts[type].text = resources[type].ToString();
+            }
+        }
     }
 }
