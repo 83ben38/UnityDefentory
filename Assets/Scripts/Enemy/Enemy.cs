0c0fca2f990790b2dd2b8617687e59e9e50764c0
ben <bb14738@ms.wasatch.edu>
2025-06-18 16:06:28 -0600

    STARS DO BE STARRING

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -192,7 +192,6 @@ public class Enemy : MonoBehaviour
     {
         if (shapeType == Type.Plus)
         {
-            //healy thing
             SpriteRenderer sp = GetComponent<SpriteRenderer>();
             transform.localScale *= 2f;
             sp.color = new Color(0, 125, 0, 125);
@@ -216,6 +215,50 @@ public class Enemy : MonoBehaviour
             transform.localScale *= 0.5f;
         }
 
+        if (shapeType == Type.Star)
+        {
+            if (Random.Range(0, 4) == 0)
+            {
+                int currentD = findDistance(location);
+                List<Vector2Int> possibleDestinations = new List<Vector2Int>();
+                for (int i = -size; i <= size; i++)
+                {
+                    for (int j = -size; j <= size; j++)
+                    {
+                        int d = findDistance(new Vector2Int(location.x+i,location.y+j));
+                        if (d != -1 && d < currentD)
+                        {
+                            possibleDestinations.Add(new Vector2Int(location.x+i,location.y+j));
+                        }
+                    }
+                }
+
+                if (possibleDestinations.Count != 0)
+                {
+                    Vector2Int randomDestination = possibleDestinations[Random.Range(0, possibleDestinations.Count)];
+                    var transform1 = transform;
+                    float startingScale = transform1.localScale.x;
+                    for (float i = 1; i >= 0; i -= Time.deltaTime*3)
+                    {
+                        transform1.localScale = new Vector3(i*startingScale, i*startingScale);
+                        transform1.eulerAngles = new Vector3(0, 0, i * 360);
+                        yield return null;
+                    }
+                    transform1.position = new Vector3(randomDestination.x+ Random.value * .7f - .35f, randomDestination.y+ Random.value * .7f - .35f);
+                    location = randomDestination;
+                    for (float i = 0; i <= 1; i += Time.deltaTime*3)
+                    {
+                        transform1.localScale = new Vector3(i*startingScale, i*startingScale);
+                        transform1.eulerAngles = new Vector3(0, 0, i * 360);
+                        yield return null;
+                    }
+                    transform1.localScale = new Vector3(startingScale,startingScale);
+                    transform1.eulerAngles = new Vector3(0, 0, 0);
+                    CheckLocation(false);
+                    yield break;
+                }
+            }
+        }
         Tile on = GridController.instance.grid[location];
         Vector2Int desinationTile;
         Vector3 start = transform.position;
@@ -241,5 +284,27 @@ public class Enemy : MonoBehaviour
         transform.position = end;
         location = desinationTile;
         CheckLocation(false);
+    }
+    public int findDistance(Vector2Int start)
+    {
+        if (EnemyPathingManager.instance.isEnd(start))
+        {
+            return 0;
+        }
+        if (!GridController.instance.grid.ContainsKey(start))
+        {
+            return -1;
+        }
+        Tile t = GridController.instance.grid[start];
+        if (t.tileType != Tile.Type.EnemyBelt)
+        {
+            return -1;
+        }
+        if (t.rotation % 2 == 0)
+        {
+            return findDistance(start + new Vector2Int(0,1-t.rotation))+1;
+        }
+
+        return findDistance(start + new Vector2Int(t.rotation - 2, 0))+1;
     }
 }
